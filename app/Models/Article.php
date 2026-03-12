1f4f12bd29c9a942248515d1192f73748821dec2
dario1979 <dariobitti@gmail.com>
2026-03-12 12:39:35 -0300

    se crearon migraciones y models

---
+++ app/Models/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'status',
+        'published_at',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'article_category');
+    }
+}
